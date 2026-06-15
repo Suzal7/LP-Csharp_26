4ec08ac68d62f7fcfba33410d125102729bb3891
Suzal7 <suzaltamang679@gmail.com>
2026-06-15 11:13:59 +0100

    exercicio9

---
+++ exercicio9/exercicio9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class exercicio9
+{
+    public static void Main(string[] args)
+    {
+        for (int contador = 0; contador <= 20; contador += 2 )
+        {
+            Console.WriteLine(contador);
+        }
+         
+    }
+
+}
