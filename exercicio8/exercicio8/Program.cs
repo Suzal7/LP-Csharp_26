cc9b66afcb4d2976bf7cd554d96597ad4e1d5e32
Suzal7 <suzaltamang679@gmail.com>
2026-06-15 10:23:01 +0100

    Exercicio 8

---
+++ exercicio8/exercicio8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class exercicio8
+{
+    public static void Main(string[] args)
+    {
+        int nota = 16;
+
+        switch (nota)
+        {
+            case int n when (nota > 20 || nota < 0):
+                Console.WriteLine("Nota inválida");
+                break;
+            case int n when (nota > 17):
+                Console.WriteLine("Excelente");
+                break;
+            case int n when (nota > 13):
+                Console.WriteLine("Bom");
+                break;
+            case int n when (nota > 9):
+                Console.WriteLine("Suficiente");
+                break;
+            case int n when (nota > 0):
+                Console.WriteLine("Insuficiente");
+                break;
+            default:
+                Console.WriteLine("Nota inválida");
+                break;
+        }
+
+    }
+}
