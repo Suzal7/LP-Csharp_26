c36cd58ba50edc7fe941b52d38328945ae6d1e57
Suzal7 <suzaltamang679@gmail.com>
2026-06-15 11:47:05 +0100

    exercicio9

--- exercicio9/exercicio9/Program.cs
+++ exercicio9/exercicio9/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+
 public class exercicio9
 {
     public static void Main(string[] args)
     {
-        for (int contador = 0; contador <= 20; contador += 2 )
-        {
-            Console.WriteLine(contador);
-        }
-         
-    }
+        int par = 2;
 
+        for (int contador = 0; contador <= 20; contador++)
+        {
+            if (par == 1 && contador % 2 == 0)
+            {
+                Console.WriteLine(contador);
+            }
+            else if (par == 2 && contador % 2 != 0)
+            {
+                Console.WriteLine(contador);
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido uff");
+            }
+        }
+    }
 }
